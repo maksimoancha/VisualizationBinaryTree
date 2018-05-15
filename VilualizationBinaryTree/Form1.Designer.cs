318540431754e8f96966443edabb10fefc4a390f
Maksim Oancha <maksimoancha12@gmail.com>
2018-05-15 23:30:17 +0300

    Add project files.

---
+++ VilualizationBinaryTree/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace VilualizationBinaryTree
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddValue = new System.Windows.Forms.Button();
+            this.txbValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRemoveItem = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddValue
+            // 
+            this.btnAddValue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnAddValue.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnAddValue.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddValue.ForeColor = System.Drawing.Color.Black;
+            this.btnAddValue.Location = new System.Drawing.Point(553, 57);
+            this.btnAddValue.Name = "btnAddValue";
+            this.btnAddValue.Size = new System.Drawing.Size(100, 23);
+            this.btnAddValue.TabIndex = 0;
+            this.btnAddValue.Text = "Add Item";
+            this.btnAddValue.UseVisualStyleBackColor = false;
+            this.btnAddValue.Click += new System.EventHandler(this.btnAddValue_Click);
+            // 
+            // txbValue
+            // 
+            this.txbValue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbValue.Location = new System.Drawing.Point(553, 28);
+            this.txbValue.Name = "txbValue";
+            this.txbValue.Size = new System.Drawing.Size(100, 23);
+            this.txbValue.TabIndex = 1;
+            this.txbValue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(554, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter a Value:";
+            // 
+            // btnRemoveItem
+            // 
+            this.btnRemoveItem.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRemoveItem.BackColor = System.Drawing.Color.PaleGreen;
+            this.btnRemoveItem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRemoveItem.ForeColor = System.Drawing.Color.Black;
+            this.btnRemoveItem.Location = new System.Drawing.Point(553, 86);
+            this.btnRemoveItem.Name = "btnRemoveItem";
+            this.btnRemoveItem.Size = new System.Drawing.Size(100, 23);
+            this.btnRemoveItem.TabIndex = 3;
+            this.btnRemoveItem.Text = "Remove Item";
+            this.btnRemoveItem.UseVisualStyleBackColor = false;
+            this.btnRemoveItem.Click += new System.EventHandler(this.btnRemoveItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(554, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Change Span:";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.PaleGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(553, 147);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Increase";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.PaleGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(553, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Decrease";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(665, 331);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRemoveItem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbValue);
+            this.Controls.Add(this.btnAddValue);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "MainForm";
+            this.Text = "Visualize Binary Tree";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddValue;
+        private System.Windows.Forms.TextBox txbValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRemoveItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ VilualizationBinaryTree/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyLinkedList;
+
+namespace VilualizationBinaryTree
+{
+    public partial class MainForm : Form
+    {
+        private MyBinaryTree<int> _binaryTree = null;
+        private List<int> _listValues = null;
+        private int _levelDepth = 0;
+        private int _heightDepth = 50;
+        private int _span = 20;
+        
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+            
+            _binaryTree = new MyBinaryTree<int>();
+            _binaryTree.Add(50);
+            _binaryTree.Add(25);
+            _binaryTree.Add(70);
+            _binaryTree.Add(15);
+            _binaryTree.Add(44);
+            _binaryTree.Add(85);
+            _binaryTree.Add(71);
+            _binaryTree.Add(2);
+            _binaryTree.Add(60);
+            _binaryTree.Add(30);
+            _binaryTree.Add(45);
+            _binaryTree.Add(18);
+            _binaryTree.Add(4);
+            _binaryTree.Add(16);
+            _binaryTree.Add(99);
+            _binaryTree.Add(86);
+            _binaryTree.Add(100);
+            _binaryTree.Add(55);
+            _binaryTree.Add(65);
+            
+            Invalidate();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //Draw a stencil lines
+            for (int i = 0; i < 10; ++i)
+            {
+                float[] dashValues = { 5, 2, 15, 4 };
+                Pen blackPen = new Pen(Color.Gray, 1);
+                blackPen.DashPattern = dashValues;
+
+                e.Graphics.DrawLine(blackPen,
+                    0,
+                    _heightDepth * i + 10,
+                    ClientSize.Width,
+                    _heightDepth * i + 10
+                );
+            }
+
+            _listValues = new List<int>();
+            foreach (var val in _binaryTree)
+            {
+                _listValues.Add(val);
+            }
+
+            //Draw a binary tree
+            _levelDepth = 0;
+            DrawBinaryTree(e.Graphics);
+        }
+
+        private void DrawBinaryTree(Graphics gr)
+        {
+            DrawNodeAndRibs(@gr, _binaryTree.RootItem);
+            ++_levelDepth;
+            List<BinaryTreeItem<int>> list = new List<BinaryTreeItem<int>>();
+            list.Add(_binaryTree.RootItem);
+            int drawnNodes = 1;
+            while (drawnNodes < _listValues.Count)
+            {
+                foreach (var item in list) //draw all nodes at this level
+                {
+                    if (item.Left != null)
+                        DrawNodeAndRibs(gr, item.Left);
+                    if (item.Rigth != null)
+                        DrawNodeAndRibs(gr, item.Rigth);
+                }
+
+                int count = list.Count; //fix count of items at current level of Depth
+
+                for (int i = 0; i < count; i++) //add to list all items of next level Depth
+                {
+                    if (list[i].Left != null)
+                    {
+                        list.Add(list[i].Left);
+                        drawnNodes++;
+                    }
+                    if (list[i].Rigth != null)
+                    {
+                        list.Add(list[i].Rigth);
+                        drawnNodes++;
+                    }
+                }
+                list.RemoveRange(0, count); //remove from list all items current level of Depth
+                ++_levelDepth;//increment level of Depth
+            }
+        }
+
+        private void DrawNodeAndRibs(Graphics gr, BinaryTreeItem<int> node)
+        {
+            if (node.Left != null)
+            {
+                Pen myPen = new Pen(Color.Red, 4);
+                gr.DrawLine(myPen,
+                    _span * _listValues.IndexOf(node.Value) + 10,
+                    _levelDepth * _heightDepth + 10,
+                    _span * _listValues.IndexOf(node.Left.Value) + 10,
+                    (_levelDepth + 1) * _heightDepth + 10
+                );
+            }
+
+            if (node.Rigth != null)
+            {
+                Pen myPen = new Pen(Color.Red, 4);
+                gr.DrawLine(myPen,
+                    _span * _listValues.IndexOf(node.Value) + 10,
+                    _levelDepth * _heightDepth + 10,
+                    _span * _listValues.IndexOf(node.Rigth.Value) + 10,
+                    (_levelDepth + 1) * _heightDepth + 10
+                );
+            }
+
+            gr.FillEllipse(
+                new SolidBrush(Color.Green),
+                (_span * _listValues.IndexOf(node.Value)-10),
+                _levelDepth * _heightDepth-10,
+                40, 40
+            );
+
+            gr.DrawString(
+                node.Value.ToString(),
+                this.Font, 
+                new SolidBrush(Color.White),
+                (_span * _listValues.IndexOf(node.Value)+10F) - gr.MeasureString(node.Value.ToString(), this.Font).Width/2F,
+                (_levelDepth * _heightDepth+10) - (int)gr.MeasureString(node.Value.ToString(), this.Font).Height/2
+            );
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void btnAddValue_Click(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(txbValue.Text, out int newValue))
+            {
+                _binaryTree.Add(newValue);
+                Invalidate();
+            }
+        }
+
+        private void btnRemoveItem_Click(object sender, EventArgs e)
+        {
+            if (Int32.TryParse(txbValue.Text, out int value))
+            {
+                _binaryTree.RemoveByValue(value);
+                Invalidate();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _span += 5;
+            Invalidate();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_span > 20)
+                _span -= 5;
+            Invalidate();
+        }
+    }
+}
